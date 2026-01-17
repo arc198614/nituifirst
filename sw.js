19f0db7abda95dd7523fd9f6e843741ccfe98c3e
arc198614 <mazakaranja@gmail.com>
2026-01-17 19:59:06 +0530

    Enhance notification reliability for Android PWA

--- sw.js
+++ sw.js
@@ -9,10 +9,17 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    // Skip waiting to activate new SW immediately
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
     );
+});
+
+self.addEventListener('activate', event => {
+    // Claim clients immediately to control the page
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
